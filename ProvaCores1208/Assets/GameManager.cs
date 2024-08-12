fe2e34727bfd17f5af67e7658190478c853d02d2
NicolasXd36MlM <yukiminko519@gmail.com>
2024-08-12 10:05:08 -0300

    declarar Variaveis
    Nicolas Bryan

--- ProvaCores1208/Assets/GameManager.cs
+++ ProvaCores1208/Assets/GameManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    #region Singleton
+    int corDaVez, acertos, erros;
+    int[] sequencia;
+    [SerializeField]
+    string[] nomes;
+    #endregion
+
     // Start is called before the first frame update
     void Start()
     {
