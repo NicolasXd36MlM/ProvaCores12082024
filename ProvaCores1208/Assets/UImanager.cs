d5bfde532d9e5241e6d4217dcc46915456377b93
NicolasXd36MlM <yukiminko519@gmail.com>
2024-08-12 09:04:03 -0300

    UImanager Metodo Start e laço for
    NicolasB

---
+++ ProvaCores1208/Assets/UImanager.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+    #region Singleton
+    Button[] botoes;
+    [SerializeField]
+    TextMeshProUGUI sequenciaTexto, acertouTexto, errouTexto;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int  i= 0; i < botoes.Length; i++)
+        {
+            // botoes[i].GameManager.ChecarCor.onClick;
+           
+        }
+    }
+
+    public void atualizarAcertos ()
+    {
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
