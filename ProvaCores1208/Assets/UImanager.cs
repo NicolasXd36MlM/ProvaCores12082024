98c0c8668cf240015582f3f3096f1f5e02119638
NicolasXd36MlM <yukiminko519@gmail.com>
2024-08-12 10:12:57 -0300

    adição do UImanager
    Nicolas

---
+++ ProvaCores1208/Assets/UImanager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+    #region Singleton
+    Button[] botoes;
+    [SerializeField]
+    TextMeshProUGUI sequenciaTexto, acertouTexto, errouTexto;
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < botoes.Length; i++)
+        {
+            // botoes[i].GameManager.ChecarCor.onClick;
+
+        }
+    }
+
+    public void atualizarAcertos(int acertouTexto) => acertouTexto.ToString();
+
+    public void atualizarErros(int errouTexto) => errouTexto.ToString();
+
+    public void LimparTexto(int texto) => texto.ToString("");
+
+    public void AtualizarSequencia(string cor)
+    {
+        cor = cor.ToString();
+
+    }
+}
